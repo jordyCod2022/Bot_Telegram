17a77f472334cdf0936182ed827f59d8c27fe7f8
Jordy Bernabe <bernabejoseph79@gmail.com>
2024-02-10 21:45:08 -0500

    nuevo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connectionTimeoutMillis = 40000;
 
 
 const telegramToken = '6777426387:AAHvHB1oJdcMqt6hutj2D1ZqcI7y0a2dFBg';
-const telegramTokenAres = '6662132787:AAHJtUh0yzHs0VJWTNWmy6Nn30laOjM4S8M';
+const telegramTokenAres = '6709288127:AAFWDLE4qkZcpdxrrNhdSugpKIlIl6uz3VM';
 const bot = new TelegramBot(telegramToken, { polling: false });
 const botAres = new TelegramBot(telegramTokenAres, { polling: false });
 
